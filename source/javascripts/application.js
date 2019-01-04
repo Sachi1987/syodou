24a6945d365a4d98938437838a915aecc9fd15ef
sachi <sachi1210@me.com>
2019-01-04 16:24:19 -0200

    First final version

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,16 +2,16 @@
 //= require bootstrap
 //= require_tree .
 
-// What does this JS do?
+// This function enables the scroll effect.
 
 $(function(){
-$('a[href^="#"]').click(function(){
-var speed = 500;
-var href= $(this).attr("href");
-var target = $(href == "#" || href == "" ? 'html' : href);
-var position = target.offset().top;
-$("html, body").animate({scrollTop:position}, speed, "swing");
-return false;
-});
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
 });
 
